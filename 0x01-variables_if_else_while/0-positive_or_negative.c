9090ece6ab0d7d72b7684b05e574354f1e008ebe
jashjchoi <jshjchoi@gmail.com>
2020-06-11 17:44:46 +0000

    3rd update Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 /**
 * main - entry point
 *
@@ -8,7 +8,6 @@
 *
 * Return: always 0 (if success)
 */
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
