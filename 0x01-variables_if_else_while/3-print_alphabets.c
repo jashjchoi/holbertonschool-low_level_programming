7dc184ddf813dc5ba0aff16fdc5b1489ccc65b04
jashjchoi <jshjchoi@gmail.com>
2020-06-11 19:59:50 +0000

    Add task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Prints the alphabet in lower and uppercase
+*
+* Return: always 0 (if success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
