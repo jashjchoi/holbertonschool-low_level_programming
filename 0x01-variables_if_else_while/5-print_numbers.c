6ce733efbb15146f0a7e38b75eda1e83b79b7f9e
jashjchoi <jshjchoi@gmail.com>
2020-06-11 21:31:20 +0000

    Add task 5 after testing with Betty style

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: prints numbers of base 10 starting from 0
+*
+* Return: always 0 (if success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
