3730f61770980ef4667996e6a9a57f63399f3406
jashjchoi <jshjchoi@gmail.com>
2020-06-11 22:35:32 +0000

    Add task 7 after testing with Betty style

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: prints lowercase alphabet in reverse
+*
+* Return: Always 0 (if success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
