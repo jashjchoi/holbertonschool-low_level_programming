c799b8556b38d387a192e655ca48651df46adda3
jashjchoi <jshjchoi@gmail.com>
2020-06-14 22:17:03 +0000

    Add Adv. task 1 after testing Betty style

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - print different combinations
+*
+* Return: always 0 (if success)
+*/
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 48; first <= 57; first++)
+	{
+		for (second = 48; second <= 57; second++)
+		{
+			if (second > first)
+			{
+				putchar(first);
+				putchar(second);
+
+				if (first < 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return ('0');
+}
