303a6ac29b3b58fe0ba5a4a31f724cda36054538
jashjchoi <jshjchoi@gmail.com>
2020-06-16 17:32:45 +0000

    Change task 7 function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-
-	if (n < 0)
-	{
-		n = n * -1;
-	}
 	last_digit = n % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
