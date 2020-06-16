149a5de97c88b84c6f85fc0fe8a65d664bd80ff0
jashjchoi <jshjchoi@gmail.com>
2020-06-16 15:35:43 +0000

    Add task 1 after Betty test and creating 1-main.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print_alphabet - prints the lowercase
+*
+* Return: nothing
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
