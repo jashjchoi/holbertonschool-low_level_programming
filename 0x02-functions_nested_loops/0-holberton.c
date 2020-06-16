2dc6ea108e8a6df772b628678d483840bb9f2d9f
jashjchoi <jshjchoi@gmail.com>
2020-06-16 15:07:10 +0000

    Add task 0 after Betty test

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* main - prints Holberton, followed by new line
+*
+* Return: always 0
+*/
+int main(void)
+{
+	int i;
+	char string[9] = "Holberton";
+
+	for (i =0; i < 9; i++)
+	{
+		_putchar(string[i]);
+	}
+	putchar('\n');
+	return (0);
+}
