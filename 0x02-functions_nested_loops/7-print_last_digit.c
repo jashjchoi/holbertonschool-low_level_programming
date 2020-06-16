840de439e5f78f0d7c6a0f1be4be00f4832d8bdc
jashjchoi <jshjchoi@gmail.com>
2020-06-16 17:21:12 +0000

    Add task 7 after Betty test and update the header file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* print_last_digit - prints the last digit of n
+*
+* @n: num to test
+*
+* Return: the last digit of n
+*/
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	last_digit = n % 10;
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
