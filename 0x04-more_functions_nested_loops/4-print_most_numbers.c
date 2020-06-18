3c84258be538c98bb5ee5461e5497af54e21b57c
jashjchoi <jshjchoi@gmail.com>
2020-06-18 21:55:38 +0000

    Fix while loop function for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,19 +6,17 @@
 */
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (i <= 9)
 	{
-		if ((i != 2) || (i != 4))
-		{
-			_putchar(i + '0');
-			i++;
-		}
-		else
+		if ((i == 2) || (i == 4))
 		{
 			i++;
 		}
+		_putchar(i + '0');
+		i++;
 	}
 	_putchar('\n');
 }
