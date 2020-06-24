91d7075c4c3240c3362edef575f9087b34e9e4fd
jashjchoi <jshjchoi@gmail.com>
2020-06-24 03:16:01 +0000

    Fix task 7 function and syntax

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
 /**
+* puts_half - prints the second half of string
 *
+* @str: string
 *
-*
+* Return: void
 */
 void puts_half(char *str)
 {
 	int length;
+
 	length = 0;
 
 	while (*str != '\0')
