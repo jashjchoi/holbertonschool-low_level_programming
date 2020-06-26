1ab57c7b6e8fa2ef6c4259630921ebdf96825335
jashjchoi <jshjchoi@gmail.com>
2020-06-26 02:25:49 +0000

    First commit to push the header file and README.md

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+#endif /* HOLBERTON_H */
