859b7315837ace8f0dc19aee525b776a9be63cb3
jashjchoi <jshjchoi@gmail.com>
2020-07-05 22:21:27 +0000

    First commit to push Task 0, the header file, and README.md

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - prints its name, followed by a new line
+* @argc: number (or length) of command arguments
+* @argv: pointer to arguments
+* Return: 0
+*/
+int main(int argc __attribute__((unused)), char *argv[0])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
