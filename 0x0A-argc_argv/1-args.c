94e16547ad95b3ceea3db6a03ffd5ad5e44c92b9
jashjchoi <jshjchoi@gmail.com>
2020-07-05 22:30:02 +0000

    Add Task 1 after Betty test

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - prints the number of arguments passed
+* @argc: number (or length) or command arguments
+* @argv: pointer to arguments
+* Return: 0
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
