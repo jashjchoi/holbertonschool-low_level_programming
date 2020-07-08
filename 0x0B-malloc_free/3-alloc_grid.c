1dd8ed93b16d16fc7a04877ab48fa3846cabd21a
jashjchoi <jshjchoi@gmail.com>
2020-07-08 04:34:48 +0000

    Add task 3 after betty test

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* alloc_grid - prints 2-d array of int
+* @width: column of array
+* @height: row of array
+* Return: 2d array of int, NULL if fails or width(or height) <= 0
+*/
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	int j = 0;
+	int **dgrid;
+
+	dgrid = malloc(sizeof(int *) * height);
+
+	if (width <= 0)
+	{
+		return (NULL);
+	}
+	if (height <= 0)
+	{
+		return (NULL);
+	}
+	if (dgrid == NULL)
+	{
+		free(dgrid);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		dgrid[i] = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+		{
+			dgrid[i][j] = 0;
+		}
+		if (dgrid[i] == NULL)
+		{
+			for (i = i; i >= 0; i--)
+			{
+				free(dgrid[i]);
+			}
+			free(dgrid);
+			return (NULL);
+		}
+	}
+	return (dgrid);
+}
