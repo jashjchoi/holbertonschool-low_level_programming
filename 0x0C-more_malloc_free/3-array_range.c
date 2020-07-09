e7b4d17f9f458fefe06b5e37234d17ba00fcd03d
jashjchoi <jshjchoi@gmail.com>
2020-07-09 21:15:10 +0000

    Add task 3 after Betty test

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* array_range - creates an array of int
+* @min: pointer to min int
+* @max: pointer to max int
+* Return: array of integers ordered from min to max
+*/
+int *array_range(int min, int max)
+{
+	int i, size;
+	int *array;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	size = max - min + 1;
+	array = malloc(sizeof(int *) * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = min;
+		min++;
+	}
+	return (array);
+}
