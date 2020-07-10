f7b14562485660eccadf6299dd794688ab559c7b
jashjchoi <jshjchoi@gmail.com>
2020-07-10 15:31:29 +0000

    Fix the if statement Task 1 functions

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *array;
 	unsigned int i = 0, j = 0, length1, length2;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		s1 = s2 = "";
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	for (length1 = 0; s1[length1] != '\0'; length1++)
 	{
