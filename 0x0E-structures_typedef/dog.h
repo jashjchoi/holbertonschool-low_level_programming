35c35ace5ab1bfdf68edcb0b9cabe734c1192632
jashjchoi <jshjchoi@gmail.com>
2020-07-14 00:20:08 +0000

    First commit to add the header file and README.md

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct_dog - struct of dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
