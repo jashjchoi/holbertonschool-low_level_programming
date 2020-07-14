870d353f34809759bbbda752840ce6b15bc764cf
jashjchoi <jshjchoi@gmail.com>
2020-07-14 00:39:43 +0000

    Change the if statement for task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,14 +11,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	}
-	else
-	{
-		return;
 	}
 }
