57088d03f78c01f83527138db4ef86d6cc47a9a2
jashjchoi <jshjchoi@gmail.com>
2020-07-24 02:11:22 +0000

    Add task 0 after Betty test

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+* print_list - prints all the elements of a list_t
+* @h: sll
+* Return: number of nodes in the list
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil"));
+		}
+		else
+		{
+			printf("[%d] %s\n", h->length, h->str);
+			h = h->next;
+		}
+	}
+	return (i);
+}
