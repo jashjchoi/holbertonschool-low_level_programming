7c301f79c8938284f6a7f27e24fe3e573b4fc69d
jashjchoi <jshjchoi@gmail.com>
2020-07-24 05:03:11 +0000

    Fix the order of section for Adv.task 100

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,14 @@
-section   .text
+section .data
+	msg db "Hello, Holberton", 10
+
+	section .text
 	global main
 main:
-	mov	rax, 1	; system call for write
-        mov	rdi, 1	; file handle 1 is stdout
-	mov	rsi, message	; address of string "Hello, Holberton\n" to output
-	mov	rdx, 17	; number of bytes of str
-	syscall	; invoke operating system to do the write
-	mov	rax, 60	; system call for exit
-	mov	rdi, rdi	; exit code 0
-	syscall	; invoke operating system to exit
-section	.data
-msg db	"Hello, Holberton", 10	; note the newline at the end
+	mov	rax, 1
+        mov	rdi, 1
+	mov	rsi, msg
+	mov	rdx, 17
+	syscall
+	mov	rax,
+	mov	rdi, 0
+	syscall	
