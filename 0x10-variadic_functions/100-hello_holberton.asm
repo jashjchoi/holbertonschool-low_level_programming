759076eb117c4a93e16f46c978812bb61e8abdb4
jashjchoi <jshjchoi@gmail.com>
2020-07-24 04:59:31 +0000

    Update Adv.task 100

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,13 @@
-global		_hello
-		section   .text
-_hello      	mov       rax, 1                  ; system call for write
-            	mov       rdi, 1                  ; file handle 1 is stdout
-            	mov       rsi, message            ; address of string "Hello, Holberton\n" to output
-            	mov       rdx, 17                 ; number of bytes of str
-            	syscall                           ; invoke operating system to do the write
-            	mov       rax, 60                 ; system call for exit
-            	mov       rdi, rdi                ; exit code 0
-            	syscall                           ; invoke operating system to exit
-
-		section   .data
-message:	db	"Hello, World", 10      ; note the newline at the end
+section   .text
+	global main
+main:
+	mov	rax, 1	; system call for write
+        mov	rdi, 1	; file handle 1 is stdout
+	mov	rsi, message	; address of string "Hello, Holberton\n" to output
+	mov	rdx, 17	; number of bytes of str
+	syscall	; invoke operating system to do the write
+	mov	rax, 60	; system call for exit
+	mov	rdi, rdi	; exit code 0
+	syscall	; invoke operating system to exit
+section	.data
+msg db	"Hello, Holberton", 10	; note the newline at the end
