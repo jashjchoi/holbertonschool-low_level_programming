ca308642e5313e36b38cd06e528386bb8d5c5d3a
jashjchoi <jshjchoi@gmail.com>
2020-07-29 02:14:20 +0000

    Add task 7 after testing Betty standard and Valgrind

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+* get_nodeint_at_index - the nth node of a listint_t
+* @head: pointer to head of the sll
+* @index: index of the node to be printed
+* Return: pointer to nth node of sll
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node = 0;
+	listint_t *copy_h;
+
+	copy_h = head;
+
+	if (copy_h == NULL)
+	{
+		return (0);
+	}
+	if (copy_h != NULL)
+	{
+		for (node = 0; node < index; node++)
+		{
+			copy_h = copy_h->next;
+		}
+		return (copy_h);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
