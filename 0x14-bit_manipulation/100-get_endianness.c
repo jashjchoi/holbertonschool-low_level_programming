cfa26d77b370ba8747cf7ad85e10b1e99e8e7c50
jashjchoi <jshjchoi@gmail.com>
2020-08-27 16:15:10 +0000

    Add adv. task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* get_endianness - checks the endianness of given number
+* Return: 1 if little endian, 0 if big endian
+*
+*/
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c = (char *) &x;
+
+	if (*c != NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
