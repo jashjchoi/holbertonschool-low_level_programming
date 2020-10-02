c94766afd5962bf5b0a2d5fc97dca35842bf14d9
jashjchoi <jashjchoi@gmail.com>
2020-10-02 23:13:40 +0000

    Fix the memory leak

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,8 +6,7 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	unsigned long int index;
+	hash_table_t *new_table = NULL;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
@@ -16,14 +15,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->array == NULL)
-	{
-		return (NULL);
-	}
-	while (index < size)
-	{
-		new_table->array[index] = NULL;
-		index++;
-	}
 	return (new_table);
 }
