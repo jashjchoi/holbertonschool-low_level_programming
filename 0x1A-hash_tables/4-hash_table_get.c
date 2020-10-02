56c3bf3a6e9021d8d4291486ee3f5b93d90a8d9f
jashjchoi <jashjchoi@gmail.com>
2020-10-02 22:42:48 +0000

    Fix the syntax issues

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,7 +3,7 @@
 * hash_table_get - retrieves a value associated with a key
 * @ht: the hash table you want to look into
 * @key: key to look for
-* Return: value associated with the element, or NULL if key not found
+* Return: value, or NULL if key not found
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
@@ -11,22 +11,22 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *temp = NULL;
 
 	if (ht == NULL || key == NULL)
-    {
-		return (NULL);
-    }
-    idx = key_index((const unsigned char *)key, ht->size);
-    temp = ht->array[idx];
-    if (temp == NULL)
-    {
-        return (NULL);
-    }
-    if (temp)
 	{
-        if (strcmp(temp->key, key) == 0)
-        {
-            return (temp->value);
-        }
-        temp = temp->next;
+		return (NULL);
+	}
+	idx = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[idx];
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	if (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+		}
+		temp = temp->next;
 	}
 	return (NULL);
 }
