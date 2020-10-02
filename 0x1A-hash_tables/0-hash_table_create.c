fe0d5f81d9b99c9232bba8d9a28ff4f57862a66c
jashjchoi <jashjchoi@gmail.com>
2020-10-02 17:11:35 +0000

    Fix the syntax

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,9 +11,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-    {
+	{
 		return (NULL);
-    }
+	}
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
