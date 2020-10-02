503e798fecff0a2ab1f2d4d405df1d758fd6547f
jashjchoi <jashjchoi@gmail.com>
2020-10-02 23:07:58 +0000

    Fix the loop function and syntax

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,14 +6,24 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL;
+	hash_table_t *new_table;
+	unsigned long int index;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-    {
+	{
 		return (NULL);
-    }
-    new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);	
+	}
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		return (NULL);
+	}
+	while (index < size)
+	{
+		new_table->array[index] = NULL;
+		index++;
+	}
 	return (new_table);
 }
