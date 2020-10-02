4892ade110d5b35da4fde2035e0f945e8a2b49a6
jashjchoi <jashjchoi@gmail.com>
2020-10-02 22:53:48 +0000

    Change the while loop location

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,26 +7,25 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int idx = 0;
+	unsigned long int index = 0;
 	hash_node_t *temp = NULL;
 
-	if (ht == NULL || key == NULL)
+	if (key == NULL)
 	{
 		return (NULL);
 	}
-	idx = key_index((const unsigned char *)key, ht->size);
-	temp = ht->array[idx];
-	if (temp == NULL)
+	if (ht != NULL)
 	{
-		return (NULL);
-	}
-	if (temp)
-	{
-		if (strcmp(temp->key, key) == 0)
+		index = key_index((const unsigned char *)key, ht->size);
+		temp = ht->array[index];
+		while (temp)
 		{
-			return (temp->value);
+			if (strcmp(temp->key, key) == 0)
+			{
+				return (temp->value);
+			}
+			temp = temp->next;
 		}
-		temp = temp->next;
 	}
 	return (NULL);
 }
