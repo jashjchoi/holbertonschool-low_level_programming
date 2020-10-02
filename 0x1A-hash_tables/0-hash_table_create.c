f1165a6decc9b9d955bf32f227f16614c5385fac
jashjchoi <jashjchoi@gmail.com>
2020-10-02 23:02:54 +0000

    Memory check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,10 +10,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-	{
+    {
 		return (NULL);
-	}
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+    }
+    new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);	
 	return (new_table);
 }
